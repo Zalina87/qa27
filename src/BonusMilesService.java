79301cfa51519529214f944a68f2ed8de77333ca
Zalina <salina87@yandex.ru>
2024-10-08 16:05:24 +0300

    ...

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
     public int calculate(int weight, int height) {
         int index;
-// weight - вес, height - рост
+// weight - вес, height - рост...
         index =; int weight/Math.pow(height, 2);
 
         return index;
